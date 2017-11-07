2235ab1bf882a744cbf0dfa34a5807cb679fad39
hcx <1358060427@qq.com>
2017-11-07 14:16:48 +0800

    代码

---
+++ Phone/src/constans/FetchApiUrl.js
@@ -0,0 +1,12 @@
+// export const SHOPCARTURL = "http://localhost:3000/shopcartapi";
+"use strict"
+export default {
+  "CartFetchURL":"http://localhost:3000/productapi",
+  "ProductsGiftURL":"http://localhost:3000/products/ProductsGiftApi",
+  "ProductsIntegalURL":"http://localhost:3000/products/ProductsIntegalApi",
+  "ObjectiveListLeftURL":"http://localhost:3000/products/ObjectiveListLeftApi",
+  "ObjectiveListRightURL":"http://localhost:3000/products/ObjectiveListRightApi",
+  "EggTotleNumApiURL":"http://localhost:3000/products/EggTotleNumApi",
+}
+
+
